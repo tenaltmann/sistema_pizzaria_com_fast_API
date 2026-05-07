ae72fb530463eded1fb30ea41c131b37594f5620
Ricardo Altmann <tenaltmann@gmail.com>
2026-05-06 21:53:59 -0300

    criação do schemas.py para criação das classe que irão padronizar os parametros que serão usados nas funções das rotas

--- auth_routes.py
+++ auth_routes.py
@@ -2,6 +2,12 @@ from fastapi import APIRouter, Depends, HTTPException       # importando roteado
 from models import Usuario      # importando modelo de usuario para criar conta
 from dependencies import pegar_sessao # importando função para pegar sessão de banco de dados
 from main import bcrypt_context # importando contexto de criptografia para senhas do main.py
+from schemas import UsuarioSchema # importando schema de usuario para validação dos dados de entrada
+from sqlalchemy.orm import Session # importando Session do SQLAlchemy para tipagem da sessão de banco de dados
+
+
+
+
 
 
 auth_router = APIRouter(prefix= "/auth", tags=["auth"])    # definindo prefixo padrao da rota
@@ -21,13 +27,13 @@ async def home():
         return {"mensagem":"Você acessou a rota padrão de autenticação", "autenticado": False}
 
 @auth_router.post("/criar_conta")
-async def criar_conta(email: str, senha: str, nome: str, session = Depends(pegar_sessao)):
-        usuario = session.query(Usuario).filter(Usuario.email==email).first() # verificando se o email já existe no banco de dados
+async def criar_conta(usuario_schema: UsuarioSchema  ,session: Session = Depends(pegar_sessao)):
+        usuario = session.query(Usuario).filter(Usuario.email==usuario_schema.email).first() # verificando se o email já existe no banco de dados
         if usuario:
                 raise HTTPException(status_code=400, detail="Email já cadastrado")      # se o email já existir, retorna um erro 400 (Bad Request) com a mensagem "Email já cadastrado" 
         else:
-                senha_criptografada = bcrypt_context.hash(senha) # criptografando a senha utilizando o contexto de criptografia definido no main.py             
-                novo_usuario = Usuario(nome, email, senha_criptografada) # criando novo usuário
+                senha_criptografada = bcrypt_context.hash(usuario_schema.senha) # criptografando a senha utilizando o contexto de criptografia definido no main.py             
+                novo_usuario = Usuario(usuario_schema.nome, usuario_schema.email, senha_criptografada, usuario_schema.ativo, usuario_schema.admin) # criando novo usuário
                 session.add(novo_usuario) # adicionando novo usuário na sessão
                 session.commit() # salvando alterações no banco de dados    
-                return {"mensagem": "Usuário cadastrado com sucesso"}
+                return {"mensagem": f"Usuário cadastrado com sucesso {usuario_schema.email}"} 
---
+++ schemas.py
@@ -0,0 +1,12 @@
+from pydantic import BaseModel      # Importando a classe BaseModel do módulo pydantic, que é usada para criar modelos de dados para validação e serialização.
+from typing import Optional     # Importando a classe Optional do módulo typing, que é usada para indicar que um campo é opcional em um modelo de dados.
+
+class UsuarioSchema(BaseModel):     # Criando a classe UsuarioSchema que herda de BaseModel, essa classe será usada para definir a estrutura dos dados de um usuário, incluindo os campos nome, email, senha, ativo e admin. Os campos ativo e admin são opcionais, o que significa que eles podem ser omitidos ao criar um novo usuário.
+    nome: str
+    email: str
+    senha: str
+    ativo: Optional[bool] 
+    admin: Optional[bool] 
+
+    class Config:
+        from_attributes = True      # Configurando a classe Config para permitir a criação de instâncias do modelo a partir de objetos que possuem atributos correspondentes aos campos definidos no modelo. Isso é útil para facilitar a conversão de objetos do banco de dados em instâncias do modelo, permitindo que os dados sejam validados e serializados corretamente.
