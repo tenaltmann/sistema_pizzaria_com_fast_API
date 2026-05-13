5049c4a18b5c9780fccd6d028c10fc93abb63371
Ricardo Altmann <tenaltmann@gmail.com>
2026-05-13 15:42:02 -0300

    Configurando autenticação Oauth2 com Tokens BAerer no FastAPI

--- dependencies.py
+++ dependencies.py
@@ -1,8 +1,9 @@
-from fastapi import Depends
+from fastapi import Depends, HTTPException
 from models import db      # importando modelo de usuario para criar conta
 from sqlalchemy.orm import sessionmaker, Session # importando sessionmaker para criar sessão de banco de dados  
 from models import Usuario
-
+from main import ALGORITHM, SECRET_KEY, oauth2_schema # importando contexto de criptografia e variáveis de ambiente do main.py para utilização na criação de token e autenticação de usuário
+from jose import JWTError, jwt # importando JWTError e jwt da biblioteca jose para criação e verificação de tokens JWT
 
 
 def pegar_sessao():
@@ -15,8 +16,16 @@ def pegar_sessao():
         session.close() # fechando sessão após uso
 
 
-def verificar_token(token, session: Session = Depends(pegar_sessao)):
+def verificar_token(token: str = Depends(oauth2_schema), session: Session = Depends(pegar_sessao)):
+        try:
+            dic_info = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+            id_usuario = dic_info.get("sub") # extraindo o id do usuário do token decodificado
+        except JWTError:
+            raise HTTPException(status_code=401, detail="Acesso Negado, verifique a validade do seu token") # se o token for inválido, retorna um erro 401 (Unauthorized) com a mensagem "Acesso Negado,verifique a validade do seu token"    
+        
         #verificar se o token é válido
         #caso valido, extrai o id do usuario do token
-        usuario = session.query(Usuario).filter(Usuario.id==1).first() 
+        usuario = session.query(Usuario).filter(Usuario.id==id_usuario).first() 
+        if not usuario:
+            raise HTTPException(status_code=401, detail="Acesso Inválido") # se o usuário não existir, retorna um erro 401 (Unauthorized) com a mensagem "Acesso Inválido"
         return usuario
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.security import OAuth2PasswordBearer
 from passlib.context import CryptContext
 from dotenv import load_dotenv
 import os
@@ -15,6 +16,8 @@ app = FastAPI()
 # SEMPRE Importar rotas após o "app = FastAPI()" para evitar erro de "referência circular"  
 
 bcrypt_context = CryptContext(schemes=["bcrypt"], deprecated="auto") # Configurando o contexto de criptografia para senhas
+oauth2_schema = OAuth2PasswordBearer(tokenUrl="auth/login") # Configurando o esquema de autenticação OAuth2, definindo a URL para obtenção do token de acesso (neste caso, "auth/login")
+
 
 from auth_routes import auth_router     # mudar para routeR não repetir mesmo nome 
 from order_routes import order_router   # mudar para routeR não repetir mesmo nome
